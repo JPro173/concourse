d65e586c4ca642c2f989723eb255026e5141d1b9
Kostya Cholak <kostya.cholak@anvileight.com>
2017-03-16 22:48:36 +0200

    Update deploy job

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 set -x -e
 echo [+] Deploy is running ...
 apt update
-apt install wget
+apt install -y wget
 wget https://github.com/JPro173/concourse/raw/master/html.tar.gz
 tar -xf html.tar.gz
 ls -la .
