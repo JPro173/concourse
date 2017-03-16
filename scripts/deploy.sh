d3cb91af5f7717a2247bc82a623c5be0cbbaed97
Kostya Cholak <kostya.cholak@anvileight.com>
2017-03-16 22:45:41 +0200

    Change host source

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -x -e
 echo [+] Deploy is running ...
-apk add --update ca-certificates openssl && update-ca-certificates
 wget https://github.com/JPro173/concourse/raw/master/html.tar.gz
 tar -xf html.tar.gz
 ls -la .
