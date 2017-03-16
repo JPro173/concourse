6b436e298db770f11090c01cac50ae5865ec7d21
Kostya Cholak <kostya.cholak@anvileight.com>
2017-03-16 22:44:18 +0200

    Add https support

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -x -e
 echo [+] Deploy is running ...
+apk add --update ca-certificates openssl && update-ca-certificates
 wget https://github.com/JPro173/concourse/raw/master/html.tar.gz
 tar -xf html.tar.gz
 ls -la .
