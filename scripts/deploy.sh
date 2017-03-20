5b26b36f458509c44dd5651618fed9d4610b69cd
Kostya Cholak <kostya.cholak@anvileight.com>
2017-03-20 12:54:44 +0200

    Update deploy job

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,9 +11,9 @@ ls -la .
 alias scp="sshpass -p $PASSWORD scp -o StrictHostKeyChecking=no"
 alias ssh="sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no"
 
-ssh $HOST "mkdir $build_path"
+ssh $HOST "mkdir -p $build_path"
 
-scp project/html.tar.gz $HOST:$build_path
+scp project/html.tar.gz $HOST:$build_path/html.tar.gz
 scp project/scripts/run.sh $HOST:run.sh
 
 ssh $HOST "bash run.sh $build_path"
