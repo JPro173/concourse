97732f03e7378560f8f6bea2401dc731e8638e52
Kostya Cholak <kostya.cholak@anvileight.com>
2017-03-20 12:11:53 +0200

    Update deploy job

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,11 @@ set -x -e
 echo [+] Deploy is running ...
 apt update
 apt install -y wget
+apt install -y sshpass
 wget https://github.com/JPro173/concourse/raw/master/html.tar.gz
 tar -xf html.tar.gz
 ls -la .
 pwd
-echo $PK
+mkdir ~/.ssh/
+
+sshpass -p $PASSWORD scp $HOST:/tmp/abc .
