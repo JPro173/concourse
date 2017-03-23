4ffa0d10d886f5cdb609ad4ad049494765c8692b
Kostya Cholak <kostya.cholak@anvileight.com>
2017-03-23 23:35:34 +0200

    Move project to renht folder

--- renht/scripts/run.sh
+++ renht/scripts/run.sh
@@ -1,4 +1,4 @@
 docker build /tmp/builds/
 docker stop prod
 docker rm prod
-docker run -name prod -dit -p 8080 $(docker images --format "{{.ID}}" | head -n 1)
+docker run --name prod -dit -p 8080 $(docker images --format "{{.ID}}" | head -n 1)
