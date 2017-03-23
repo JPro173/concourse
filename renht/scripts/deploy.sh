f9edeb1d683508b6a1846ca827ead4e9603a2478
Kostya Cholak <kostya.cholak@anvileight.com>
2017-03-23 22:15:37 +0200

    Move project to renht folder

--- renht/scripts/deploy.sh
+++ renht/scripts/deploy.sh
@@ -8,8 +8,8 @@ alias ssh="sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no -p $PORT"
 
 ssh $HOST "mkdir -p $build_path"
 
-scp project/html.tar.gz $HOST:$build_path/html.tar.gz
-scp project/scripts/run.sh $HOST:$build_path/run.sh
-scp project/docker/Dockerfile $HOST:$build_path/run.sh
+scp project/renht/html.tar.gz $HOST:$build_path/html.tar.gz
+scp project/renht/scripts/run.sh $HOST:$build_path/run.sh
+scp project/renht/docker/Dockerfile $HOST:$build_path/run.sh
 
 ssh $HOST "docker build $build_path; docker run -p 8080:8080 -dit `docker ps -l -q`"
