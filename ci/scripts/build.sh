40a8032321e8a53f5144732568b8c391639053cc
Kostya Cholak <kostya.cholak@anvileight.com>
2017-03-28 18:17:14 +0300

    Update assets

---
+++ ci/scripts/build.sh
@@ -0,0 +1,7 @@
+mkdir -p artifact
+
+cd project
+tar -zcvf html.tar.gz html
+cd ..
+
+mv project/html.tar.gz artifact/
--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -17,9 +17,9 @@ alias ssh="sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no -p $PORT"
 
 ssh $HOST "mkdir -p $build_path"
 
-scp project/renht/html.tar.gz $HOST:$build_path/html.tar.gz
-scp project/renht/scripts/run.sh $HOST:$build_path/run.sh
-scp project/renht/docker/start.sh $HOST:$build_path/start.sh
-scp project/renht/docker/Dockerfile $HOST:$build_path/Dockerfile
+scp artifact/html.tar.gz $HOST:$build_path/html.tar.gz
+scp project/scripts/run.sh $HOST:$build_path/run.sh
+scp project/docker/start.sh $HOST:$build_path/start.sh
+scp project/docker/Dockerfile $HOST:$build_path/Dockerfile
 
 ssh $HOST 'bash /tmp/builds/run.sh'
