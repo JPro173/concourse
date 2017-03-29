2bb572283d18ca19d2ce7fa0f339a6018b59b76b
Kostya Cholak <kostya.cholak@anvileight.com>
2017-03-29 16:28:34 +0300

    Update assets

--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -16,30 +16,24 @@ echo "[+] Settings up SSH"
 
 pwd
 mkdir -p ~/.ssh/
-echo ${PK} > /root/.ssh/id_rsa_no_pf
-echo "$PBK" > /root/.ssh/id_rsa_no_pf.pub
+echo "$PK" > /root/.ssh/id_rsa
 
-chmod 600 ~/.ssh/id_rsa_no_pf
-chmod 600 ~/.ssh/id_rsa_no_pf.pub
+chmod 600 ~/.ssh/id_rsa
 chmod 700 ~/.ssh/
 chmod 700 ~
-
-cat /etc/ssh/ssh_host_rsa_key
 
 echo "[+] Done\n"
 
 eval `ssh-agent -s`
 
-ssh -i /root/.ssh/id_rsa_no_pf  -vvv -o StrictHostKeyChecking=no injectyy@45.32.236.15 "touch 123"
+alias scp="scp -o StrictHostKeyChecking=no -P $PORT"
+alias ssh="ssh -o StrictHostKeyChecking=no -p $PORT $HOST"
 
-alias scp="scp -o StrictHostKeyChecking=no -o StrictModes=no -P $PORT"
-alias ssh="python project/ci/scripts/ssh.py"
+ssh "mkdir -p $build_path"
 
-ssh $HOST "touch 1245"
+scp artifact/html.tar.gz $HOST:$build_path/html.tar.gz
+scp project/scripts/run.sh $HOST:$build_path/run.sh
+scp project/docker/start.sh $HOST:$build_path/start.sh
+scp project/docker/Dockerfile $HOST:$build_path/Dockerfile
 
-#scp artifact/html.tar.gz $HOST:$build_path/html.tar.gz
-#scp project/scripts/run.sh $HOST:$build_path/run.sh
-#scp project/docker/start.sh $HOST:$build_path/start.sh
-#scp project/docker/Dockerfile $HOST:$build_path/Dockerfile
-
-#ssh $HOST 'bash /tmp/builds/run.sh'
+ssh 'bash /tmp/builds/run.sh'
