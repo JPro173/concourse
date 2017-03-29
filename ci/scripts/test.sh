7cdc2098ff866a97ecfd5ab1cb8ff955b2d282f8
Kostya Cholak <kostya.cholak@anvileight.com>
2017-03-29 17:59:27 +0300

    Update project

--- ci/scripts/test.sh
+++ ci/scripts/test.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 cd project
 
-echo "\033[31m][+] Installing project requirements\x1B[0m]"
+echo -e "\033[31m][+] Installing project requirements\x1B[0m]"
 
 pip install -r requirements.txt
 
-echo "\033[31m][+] Done\x1B[0m]\n"
+echo -e "\033[31m][+] Done\x1B[0m]\n"
 
-echo "\033[31m][+] Running tests\x1B[0m]\n"
+echo -e "\033[31m][+] Running tests\x1B[0m]\n"
 
-echo "\033[31m][+] Running test.sh\x1B[0m]"
+echo -e "\033[31m][+] Running test.sh\x1B[0m]"
 ./test.sh
 
-echo "\n\033[31m][+] Test passed\x1B[0m]"
+echo -e "\n\033[31m][+] Test passed\x1B[0m]"
