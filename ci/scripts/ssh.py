715b34b14bbe09c8140b77cec3ab0eb095e29c5f
Kostya Cholak <kostya.cholak@anvileight.com>
2017-03-29 14:03:12 +0300

    Update assets

--- ci/scripts/ssh.py
+++ ci/scripts/ssh.py
@@ -8,4 +8,5 @@ host = sys.argv[1]
 cmd = sys.argv[2]
 
 child = pexpect.spawn('ssh -o StrictHostKeyChecking=no {} {}'.format(host, cmd))
-print(child.readline())
+for i in range(10):
+    print(child.readline())
