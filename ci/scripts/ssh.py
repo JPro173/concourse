734b438339a0a7b0ccd5f1be5423d164f360205f
Kostya Cholak <kostya.cholak@anvileight.com>
2017-03-29 14:43:22 +0300

    Update assets

--- ci/scripts/ssh.py
+++ ci/scripts/ssh.py
@@ -8,6 +8,6 @@ host = sys.argv[1]
 cmd = sys.argv[2]
 
 child = pexpect.spawn('ssh -i /root/.ssh/id_rsa_no_pf -o StrictHostKeyChecking=no {} {}'.format(host, cmd))
-child = pexpect.spawn('ssh -i /root/.ssh/id_rsa_no_pf -o StrictHostKeyChecking=no {} {}'.format(host, cmd))
 print(child.readline())
+child = pexpect.spawn('ssh -i /root/.ssh/id_rsa_no_pf -o StrictHostKeyChecking=no {} {}'.format(host, cmd))
 print(child.readline())
