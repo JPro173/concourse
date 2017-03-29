d8da16b3fb770ad3c445dd74e4c5c581cef453fc
Kostya Cholak <kostya.cholak@anvileight.com>
2017-03-29 15:02:24 +0300

    Update assets

--- ci/scripts/ssh.py
+++ ci/scripts/ssh.py
@@ -10,6 +10,7 @@ cmd = sys.argv[2]
 child = pexpect.spawn('ssh -i /root/.ssh/id_rsa_no_pf -o StrictHostKeyChecking=no injectyy@45.32.236.15 {}'.format(host, cmd))
 print(child.readline())
 child = pexpect.spawn('ssh -i /root/.ssh/id_rsa_no_pf -o StrictHostKeyChecking=no injectyy@45.32.236.15 "touch 123"'.format(host, cmd))
+child.expect("Enter passphrase for key '/root/.ssh/id_rsa_no_pf': ")
 child.send('\n')
 print(child.readline())
 print(child.readline())
