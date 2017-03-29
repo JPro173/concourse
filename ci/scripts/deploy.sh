e1431d62dc91cf37f4243a185961249dc01b20db
Kostya Cholak <kostya.cholak@anvileight.com>
2017-03-29 16:30:09 +0300

    Update assets

--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -5,16 +5,10 @@ build_path=/tmp/builds/
 
 echo "[+] Installing dependencies"
 
-pip install pexpect
-
-apt update
-apt install -y ssh openssl
-
 echo "[+] Done\n"
 
 echo "[+] Settings up SSH"
 
-pwd
 mkdir -p ~/.ssh/
 echo "$PK" > /root/.ssh/id_rsa
 
@@ -24,16 +18,14 @@ chmod 700 ~
 
 echo "[+] Done\n"
 
-eval `ssh-agent -s`
-
 alias scp="scp -o StrictHostKeyChecking=no -P $PORT"
 alias ssh="ssh -o StrictHostKeyChecking=no -p $PORT $HOST"
 
 ssh "mkdir -p $build_path"
 
 scp artifact/html.tar.gz $HOST:$build_path/html.tar.gz
-scp project/scripts/run.sh $HOST:$build_path/run.sh
-scp project/docker/start.sh $HOST:$build_path/start.sh
-scp project/docker/Dockerfile $HOST:$build_path/Dockerfile
+scp project/ci/scripts/run.sh $HOST:$build_path/run.sh
+scp project/ci/docker/start.sh $HOST:$build_path/start.sh
+scp project/ci/docker/Dockerfile $HOST:$build_path/Dockerfile
 
 ssh 'bash /tmp/builds/run.sh'
