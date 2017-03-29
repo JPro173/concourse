34b6e46a4969000929244ef1fab7966455c2d9b7
Kostya Cholak <kostya.cholak@anvileight.com>
2017-03-29 13:54:53 +0300

    Update assets

--- ci/scripts/ssh.py
+++ ci/scripts/ssh.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 import sys
-import subprocess
+import pexpect
 
-def subprocessCall(args):
-    cp = subprocess.run(args, stdout=subprocess.PIPE, universal_newlines=True)
-    cp.check_returncode()
-    return cp
 
 host = sys.argv[1]
 
 cmd = sys.argv[2]
 
-print(subprocessCall(["ssh", host, cmd]).stdout, file=sys.stderr)
+child = pexpect.spawn('ssh {} {}'.format(host, cmd))
+print(child.readline())
