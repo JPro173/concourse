a91bdc2261b8a87f9b2825cc50498995fc7ae9e9
Kostya Cholak <kostya.cholak@anvileight.com>
2017-03-29 18:21:05 +0300

    Update project

--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -3,7 +3,7 @@ set -eu
 
 build_path=/tmp/builds/
 
-echo "[+] Settings up SSH"
+printf "\033[1;32m[+] Setting up SSH\033[0m\n"
 
 mkdir -p ~/.ssh/
 echo "$PK" > /root/.ssh/id_rsa
@@ -12,16 +12,20 @@ chmod 600 ~/.ssh/id_rsa
 chmod 700 ~/.ssh/
 chmod 700 ~
 
-echo "[+] Done\n"
+printf "\033[1;32m[+] Done\033[0m\n\n"
 
 alias scp="scp -o StrictHostKeyChecking=no -P $PORT"
 alias ssh="ssh -o StrictHostKeyChecking=no -p $PORT $HOST"
 
-ssh "mkdir -p $build_path"
 
+printf "\033[1;32m[+] Transporting files on server\033[0m\n"
+ssh "mkdir -p $build_path"
 scp artifact/project.tar.gz $HOST:$build_path/project.tar.gz
 scp project/ci/scripts/run.sh $HOST:$build_path/run.sh
 scp project/ci/docker/start.sh $HOST:$build_path/start.sh
 scp project/ci/docker/Dockerfile $HOST:$build_path/Dockerfile
+printf "\033[1;32m[+] Done\033[0m\n\n"
 
+printf "\033[1;32m[+] Deploying\033[0m\n"
 ssh 'bash /tmp/builds/run.sh'
+printf "\033[1;32m[+] Done\033[0m\n"
