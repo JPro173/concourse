0a54be07232a616a27518e9947574774d7707b60
Kostya Cholak <kostya.cholak@anvileight.com>
2017-03-30 13:53:24 +0300

    Update project

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,12 +1,1 @@
 #!/bin/sh
-mkdir -p artifact
-
-cd project
-
-printf "\033[1;32m[+] Building docker image\033[0m\n"
-docker build .
-printf "\033[1;32m[+] Done\033[0m\n"
-
-cd ..
-
-mv project/project.tar.gz artifact/
