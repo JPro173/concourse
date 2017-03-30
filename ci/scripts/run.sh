082faec148b51aef6e5219b32091e53281a7a2c5
Kostya Cholak <kostya.cholak@anvileight.com>
2017-03-30 16:42:45 +0300

    Update project

--- ci/scripts/run.sh
+++ ci/scripts/run.sh
@@ -2,6 +2,10 @@ blue="\033[1;34m"
 red="\033[1;31m"
 none="\033[1;0m\n"
 
+printf "$blue[+] Downloading new image$none"
+docker pull injectyy/counter:latest
+printf "$blue[+] Done$none"
+
 printf "$blue[+] Stopping old container$none"
 docker stop prod > /dev/null 2>&1
 docker rm prod > /dev/null 2>&1
