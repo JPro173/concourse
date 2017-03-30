e1453bb7e4d717f124e76a7f34e8363921810512
Kostya Cholak <kostya.cholak@anvileight.com>
2017-03-30 13:16:20 +0300

    Update project

--- ci/scripts/run.sh
+++ ci/scripts/run.sh
@@ -3,5 +3,5 @@ docker build /tmp/builds/
 docker stop prod > /dev/null 2>&1
 docker rm prod > /dev/null 2>&1
 printf "\033[1;34m[+] Running docker container\033[0m\n"
-docker run --name prod -dit -p 8080:8080 $(docker images --format "{{.ID}}" | head -n 1) > /dev/null 2>&1
+docker run --name prod -d -p 8080:8080 $(docker images --format "{{.ID}}" | head -n 1) > /dev/null 2>&1
 printf "\033[1;34m[+] Container is running\033[0m\n"
