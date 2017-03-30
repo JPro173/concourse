7f71549ad193cf899bf8a269c2f91bd444883215
Kostya Cholak <kostya.cholak@anvileight.com>
2017-03-30 13:10:14 +0300

    Update project

--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 set -eu
 
+green="\033[1;32m"
+red="\033[1;31m"
+none="\033[1;0m\n"
+
 build_path=/tmp/builds/
 
-printf "\033[1;32m[+] Setting up SSH\033[0m\n"
+printf "$green[+] Setting up SSH$none"
 
 mkdir -p ~/.ssh/
 echo "$PK" > /root/.ssh/id_rsa
@@ -12,20 +16,26 @@ chmod 600 ~/.ssh/id_rsa
 chmod 700 ~/.ssh/
 chmod 700 ~
 
-printf "\033[1;32m[+] Done\033[0m\n\n"
+printf "$green[+] Done\n$none"
 
 alias scp="scp -o StrictHostKeyChecking=no -P $PORT"
 alias ssh="ssh -o StrictHostKeyChecking=no -p $PORT $HOST"
 
 
-printf "\033[1;32m[+] Transporting files on server\033[0m\n"
+printf "$green[+] Transporting files on server$red"
 ssh "mkdir -p $build_path"
 scp artifact/project.tar.gz $HOST:$build_path/project.tar.gz
 scp project/ci/scripts/run.sh $HOST:$build_path/run.sh
 scp project/ci/docker/start.sh $HOST:$build_path/start.sh
 scp project/ci/docker/Dockerfile $HOST:$build_path/Dockerfile
-printf "\033[1;32m[+] Done\033[0m\n\n"
+printf "$green[+] Done\n$none"
 
-printf "\033[1;32m[+] Deploying\033[0m\n"
+printf "$green[+] Deploying$none"
 ssh 'bash /tmp/builds/run.sh'
-printf "\033[1;32m[+] Done\033[0m\n"
+if [ $? -eq 0  ]
+then
+    printf "$green[+] Done$none"
+else
+    printf "$red[!] Deploy failed$none"
+    exit 1
+fi
